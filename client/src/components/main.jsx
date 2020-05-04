193ea65c676eb2647f1f2e5b51549cc87dedcc69
natalie <natalie@enkin.co.il>
2020-05-04 11:59:33 +0300

    aligning first main-layer

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -5,33 +5,60 @@ import Icon from "@material-ui/core/Icon";
 
 export default function Main() {
   return (
-    <div>
-      <Row>
-        <Col sm={12} md={6}>
-          <Row>
-            <Col>
+    <div className="bg-light ">
+      <Row className=" justify-content-around">
+        <Col
+          sm={12}
+          md={5}
+          className="d-flex justify-content-around align-items=center flex-column"
+        >
+          <Row className="p-1 bg-white">
+            <Col sm={1}>
               <Icon className="fas fa-dollar-sign text-success border border-success rounded-circle" />
             </Col>
-            <Col className="align-items-center">
+            <Col className="d-flex flex-column align-items-start">
               <div>12345$</div>
               <div>Total Revenu</div>
             </Col>
+            <Col></Col>
           </Row>
-          <Row>
-            <Col>
+          <Row className="p-1 bg-white">
+            <Col sm={1}>
               <Icon className="fas fa-cubes rounded-circle" />
             </Col>
-            <Col>
+            <Col className="d-flex flex-column align-items-start">
               <div>12345$</div>
               <div>revenu of the day</div>
             </Col>
+            <Col></Col>
+          </Row>
+          <Row className="p-1 bg-white">
+            <Col sm={1}>
+              <Icon className="fas fa-dollar-sign text-success border border-success rounded-circle" />
+            </Col>
+            <Col className="d-flex flex-column align-items-start">
+              <div>12345$</div>
+              <div>Total Revenu</div>
+            </Col>
+            <Col></Col>
+          </Row>
+          <Row className="p-1 bg-white">
+            <Col sm={1}>
+              <Icon className="fas fa-dollar-sign text-success border border-success rounded-circle" />
+            </Col>
+            <Col className="d-flex flex-column align-items-start">
+              <div>12345$</div>
+              <div>Total Revenu</div>
+            </Col>
+            <Col></Col>
           </Row>
         </Col>
-        <Col sm={12} md={6}>
+
+        <Col sm={12} md={5} className="bg-white">
           <Top10 />
         </Col>
       </Row>
-      <Row>
+      <Row className=" justify-content-around">
         <Col sm={12} md={6}>
           Rental By country map
         </Col>
@@ -39,7 +66,7 @@ export default function Main() {
           Rental Data In selected area Pie chart
         </Col>
       </Row>
-      <Row>
+      <Row className=" justify-content-around">
         <Col sm={12} md={6}>
           Net Incom for every month
         </Col>
