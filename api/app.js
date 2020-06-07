02a4e3932139cde0621eb256b03201e93dfac0c4
natalie <natalie@enkin.co.il>
2020-06-07 20:52:30 +0300

    small modification

--- api/app.js
+++ api/app.js
@@ -16,12 +16,11 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, "public")));
 
 // app.use("/testAPI", testAPIRouter);
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static(path.join(__dirname, "../client/build")));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use("/", express.static(path.join(__dirname, "../client/build")));
+// }
 app.use("/", express.static(path.join(__dirname, "../client/build")));
 app.use("/msqlapi", mysqlRouter);
 app.use("/users", usersRouter);
