f50616ef82a9fa08c967098c4ad6d7cac69d1281
natalie <natalie@enkin.co.il>
2020-06-07 15:53:17 +0300

    more adjustments

--- api/app.js
+++ api/app.js
@@ -21,12 +21,13 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
 
 // app.use("/testAPI", testAPIRouter);
 if (process.env.NODE_ENV === "production") {
   app.use("/", express.static(path.join(__dirname, "../client/build")));
 }
+app.use("/", express.static(path.join(__dirname, "../client/build")));
 app.use("/msqlapi", mysqlRouter);
 app.use("/users", usersRouter);
 
