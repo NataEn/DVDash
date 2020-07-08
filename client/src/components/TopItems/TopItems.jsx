c4d2094b11135bc08e47a864de1ae168d519ffac
natalie <natalie@enkin.co.il>
2020-07-08 16:51:14 +0300

    client: top item styling the table

--- client/src/components/TopItems/TopItems.jsx
+++ client/src/components/TopItems/TopItems.jsx
@@ -3,6 +3,7 @@ import "./TopItems.css";
 import noImage from "../../noImage.png";
 import { titleCase } from "../../utils";
 import { makeStyles } from "@material-ui/core/styles";
+import { useTheme } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -11,13 +12,23 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 
-const useStyles = makeStyles({
-  root: {},
-  container: {
-    boxShadow: "none",
-  },
-});
 export default function TopItems(props) {
+  const theme = useTheme();
+
+  const useStyles = makeStyles({
+    root: {},
+    container: {
+      boxShadow: "none",
+    },
+    cell: {
+      maxWidth: 150,
+    },
+    span: {
+      maxWidth: 50,
+      display: "inline-block",
+      padding: theme.spacing(1),
+    },
+  });
   const classes = useStyles();
   const filter = props.filter;
   const title = props.title;
@@ -50,17 +61,23 @@ export default function TopItems(props) {
               debugger;
               return (
                 <TableRow hover key={index}>
-                  <TableCell key={`${index}_${filter}`} align={"left"}>
+                  <TableCell
+                    key={`${index}_${filter}`}
+                    align={"left"}
+                    className={classes.cell}
+                  >
                     {/* {urls ? ( */}
                     <img
                       src={urls ? urls[row[`top_${filter}`]] : noImage}
                       alt={`${row[`top_${filter}`]} image`}
-                      width="20%"
+                      width="50%"
                     />
                     {/* ) : (
                        <div className="loader"></div>
                      ) */}
-                    {titleCase(row[`top_${filter}`])}
+                    <span className={classes.span}>
+                      {titleCase(row[`top_${filter}`])}
+                    </span>
                   </TableCell>
                   <TableCell key={`${index}_${row[filter]}`} align={"left"}>
                     {row["total_sales"]} $
