1b37503c9223636580ef69e75878b58a45f12f95
natalie <natalie@enkin.co.il>
2020-08-02 22:02:15 +0300

    client: show profile menu on signin

--- client/src/components/ProfileMenu/ProfileMenu.jsx
+++ client/src/components/ProfileMenu/ProfileMenu.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import AuthButton from "../AuthButton/AuthButton";
 import GlobalFirebase from "../../Firebase/FirebaseConfig";
 import AccountCircle from "@material-ui/icons/AccountCircle";
@@ -9,7 +10,18 @@ import IconButton from "@material-ui/core/IconButton";
 import MailIcon from "@material-ui/icons/Mail";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import Badge from "@material-ui/core/Badge";
+
+const useStyles = makeStyles((theme) => ({
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+    },
+  },
+}));
+
 const ProfileMenu = ({ isProfileMenuOpen, handleMenuClose, loggedIn }) => {
+  const classes = useStyles();
   const [item, setItem] = useState(1);
   useEffect(() => {
     setItem(2);
