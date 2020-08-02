c5233da0684d5a1aace9ebdbe60411adec76ee95
natalie <natalie@enkin.co.il>
2020-08-02 16:03:39 +0300

    client: adapting Header to Material ui suggested one

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
-import { makeStyles } from "@material-ui/core/styles";
+import { fade, makeStyles } from "@material-ui/core/styles";
 import OpenInBrowserIcon from "@material-ui/icons/OpenInBrowser";
 import logo from "../../logo_blue.png";
 import { Link as RouterLink, BrowserRouter as Router } from "react-router-dom";
@@ -8,42 +8,287 @@ import Link from "@material-ui/core/Link";
 import Typography from "@material-ui/core/Typography";
 import AuthButton from "../AuthButton/AuthButton";
 import GlobalFirebase from "../../Firebase/FirebaseConfig";
+//for another header
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import InputBase from "@material-ui/core/InputBase";
+import Badge from "@material-ui/core/Badge";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MailIcon from "@material-ui/icons/Mail";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MoreIcon from "@material-ui/icons/MoreVert";
 
 const useStyles = makeStyles((theme) => ({
+  bgColor: {
+    backgroundColor: "#282c34",
+  },
   margin: {
     margin: theme.spacing(1),
   },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: "none",
+    [theme.breakpoints.up("sm")]: {
+      display: "block",
+    },
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+    },
+  },
+  sectionMobile: {
+    display: "flex",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+  },
 }));
+// const Header = ({ loggedIn }) => {
+//   const classes = useStyles();
+
+//   return (
+//     <>
+//       <header className="App-header d-flex justify-content-between pl-2 pr-2">
+//         <img src={logo} alt="logo" className="p-2 logo" />
+//         <h4 className="d-inline-block">DVDash</h4>
+//         <Typography className={classes.margin}>
+//           <Link to="/store" component={RouterLink}>
+//             Store{" "}
+//           </Link>
+//           <Link to="/dashboard" component={RouterLink}>
+//             Dashboard
+//           </Link>
+//           {!loggedIn ? (
+//             <Link
+//               to="/login"
+//               component={RouterLink}
+//               color="secondary"
+//               size="medium"
+//               className={`${classes.margin} text-right`}
+//             >
+//               <OpenInBrowserIcon className="sign-in-icon" /> Sign-in
+//             </Link>
+//           ) : (
+//             <AuthButton onClickAuth={GlobalFirebase.logOut} text={"Logout"} />
+//           )}
+//         </Typography>
+//       </header>
+//       <PrimarySearchAppBar loggedIn={loggedIn} />
+//     </>
+//   );
+// };
+
 const Header = ({ loggedIn }) => {
   const classes = useStyles();
+  const [isProfileMenuOpen, setAnchorEl] = useState(false);
+  const [isMobileMenuOpen, setMobileMoreAnchorEl] = useState(false);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const renderMenu = (
+    <Menu
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id="primary-search-account-menu"
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isProfileMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
+
+  const renderMobileMenu = (
+    <Menu
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={"primary-search-account-menu-mobile"}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="secondary">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <p>Messages</p>
+      </MenuItem>
+      <MenuItem>
+        <IconButton aria-label="show 11 new notifications" color="inherit">
+          <Badge badgeContent={11} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <p>Notifications</p>
+      </MenuItem>
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label="account of current user"
+          aria-controls="primary-search-account-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+        <p>Profile</p>
+      </MenuItem>
+    </Menu>
+  );
 
   return (
-    <header className="App-header d-flex justify-content-between pl-2 pr-2">
-      <img src={logo} alt="logo" className="p-2 logo" />
-      <h4 className="d-inline-block">DVDash</h4>
-      <Typography className={classes.margin}>
-        <Link to="/store" component={RouterLink}>
-          Store{" "}
-        </Link>
-        <Link to="/dashboard" component={RouterLink}>
-          Dashboard
-        </Link>
-        {!loggedIn ? (
-          <Link
-            to="/login"
-            component={RouterLink}
-            color="secondary"
-            size="medium"
-            className={`${classes.margin} text-right`}
+    <div className={`${classes.grow} header`}>
+      <AppBar position="inharit" className={classes.bgColor}>
+        <Toolbar>
+          <img src={logo} alt="logo" className="p-2 logo" />
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="open drawer"
           >
-            <OpenInBrowserIcon className="sign-in-icon" /> Sign-in
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" noWrap>
+            DVDash
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
+          <div className={classes.grow} />
+          <Link to="/store" component={RouterLink}>
+            Store{" "}
           </Link>
-        ) : (
-          <AuthButton onClickAuth={GlobalFirebase.logOut} text={"Logout"} />
-        )}
-      </Typography>
-    </header>
+          <Link to="/dashboard" component={RouterLink}>
+            Dashboard
+          </Link>
+          {!loggedIn ? (
+            <Link
+              to="/login"
+              component={RouterLink}
+              color="secondary"
+              size="medium"
+              className={`${classes.margin} text-right`}
+            >
+              <OpenInBrowserIcon className="sign-in-icon" /> Sign-in
+            </Link>
+          ) : (
+            <AuthButton onClickAuth={GlobalFirebase.logOut} text={"Logout"} />
+          )}
+          <div className={classes.sectionDesktop}>
+            <IconButton aria-label="show 4 new mails" color="inherit">
+              <Badge badgeContent={4} color="secondary">
+                <MailIcon />
+              </Badge>
+            </IconButton>
+            <IconButton aria-label="show 17 new notifications" color="inherit">
+              <Badge badgeContent={17} color="secondary">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              aria-controls="primary-search-account-menu"
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              color="inherit"
+            >
+              <AccountCircle />
+            </IconButton>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={"primary-search-account-menu-mobile"}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </div>
   );
 };
-
 export default Header;
