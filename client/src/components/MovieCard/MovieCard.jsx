5dd01d179b3e0af95da06685dc1223e52e948462
natalie <natalie@enkin.co.il>
2020-08-06 10:27:37 +0300

    client: add movie-rating to movie card

--- client/src/components/MovieCard/MovieCard.jsx
+++ client/src/components/MovieCard/MovieCard.jsx
@@ -1,12 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./MovieCard.css";
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
+import MovieRating from "../MovieRating/MovieRating";
 import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
 
 const MovieCard = ({ title, imgUrl, vote }) => {
   const style = { backgroundImage: `url(${imgUrl})` };
@@ -20,7 +15,7 @@ const MovieCard = ({ title, imgUrl, vote }) => {
       <div className={"MovieCard_dark_background"}></div>
       <section className={"MovieCard_section"}>
         <h6 className={"MovieCard_h6"}>{title}</h6>
-
+        <MovieRating rating={vote} />
         <Button size="small" color="primary">
           Read More
         </Button>
---
+++ client/src/components/MovieRating/MovieRating.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./MovieRating.css";
+import Rating from "@material-ui/lab/Rating";
+
+import StarBorderIcon from "@material-ui/icons/StarBorder";
+import Box from "@material-ui/core/Box";
+
+const MovieRating = ({ rating }) => {
+  console.log("rating", rating);
+  return (
+    <div>
+      <Box
+        component="fieldset"
+        mb={0}
+        borderColor="transparent"
+        className="MovieRating_box"
+      >
+        <span>
+          <Rating
+            className="MovieRating_rating"
+            name="customized-empty"
+            value={Math.round(rating) / 2}
+            precision={0.5}
+            emptyIcon={<StarBorderIcon fontSize="small" />}
+            readOnly
+            size="small"
+            decimal
+          />
+          <p className="Rating_span">{Math.round(rating)}</p>
+        </span>
+      </Box>
+    </div>
+  );
+};
+export default MovieRating;
